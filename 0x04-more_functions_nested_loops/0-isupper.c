a9a032a107956879fb66bd16fd615fb6f42beb5d
Ayoubioussoufa <ayoubioussoufa@gmail.com>
2023-09-14 03:28:34 -0700

    0x04 nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper -  letter is upper
+ * @c: The value of the letter
+ * Return: 1 if upper or 0 if not
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
+
